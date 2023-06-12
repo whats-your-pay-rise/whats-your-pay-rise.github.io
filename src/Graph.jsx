41ce7452cb2da3d4828cebd617d25819fd7ab1ff
Harry Moss <harryjohnmoss@gmail.com>
2023-06-12 10:35:11 +0100

    Graph can be rectangular

--- src/Graph.jsx
+++ src/Graph.jsx
@@ -1,21 +1,21 @@
 function Graph(props) {
-    const dimension = 200;
+    const height = 250;
+    const width = 400;
     const axisOffset = 20;
     const barPadding = 10;
     const axisStrokeWidth = 2;
     const horizontalTextPadding = 5;
     const verticalTextPadding = 13;
 
-    const height = dimension;
-    const width = dimension;
-    const axisLength = dimension - 2 * axisOffset;
-    const barWidth = (axisLength - 3 * barPadding) / 2;
+    const xAxisLength = width - 2 * axisOffset;
+    const yAxisLength = height - 2 * axisOffset;
+    const barWidth = (xAxisLength - 3 * barPadding) / 2;
 
     return (
-        <svg height={height} width={width}>
+        <svg height={height} width={width} xmlns="http://www.w3.org/2000/svg">
             <line
                 x1={axisOffset - axisStrokeWidth}
-                x2={axisOffset + axisLength}
+                x2={axisOffset + xAxisLength}
                 y1={height - axisOffset + axisStrokeWidth / 2}
                 y2={height - axisOffset + axisStrokeWidth / 2}
                 stroke="black"
@@ -25,7 +25,7 @@ function Graph(props) {
                 x1={axisOffset - axisStrokeWidth / 2}
                 x2={axisOffset - axisStrokeWidth / 2}
                 y1={height - axisOffset + axisStrokeWidth}
-                y2={height - axisOffset - axisLength}
+                y2={height - axisOffset - yAxisLength}
                 stroke="black"
                 strokeWidth={axisStrokeWidth}
             />
@@ -33,7 +33,7 @@ function Graph(props) {
                 x1={axisOffset + barPadding + barWidth / 2}
                 x2={axisOffset + barPadding + barWidth / 2}
                 y1={height - axisOffset}
-                y2={height - axisOffset - axisLength * 0.9 * props.comparisonPay / Math.max(props.comparisonPay, props.currentPay)}
+                y2={height - axisOffset - yAxisLength * 0.9 * props.comparisonPay / Math.max(props.comparisonPay, props.currentPay)}
                 stroke="green"
                 strokeWidth={barWidth}
             />
@@ -41,7 +41,7 @@ function Graph(props) {
                 x1={axisOffset + 2 * barPadding + 3 * barWidth / 2}
                 x2={axisOffset + 2 * barPadding + 3 * barWidth / 2}
                 y1={height - axisOffset}
-                y2={height - axisOffset - axisLength * 0.9 * props.currentPay / Math.max(props.comparisonPay, props.currentPay)}
+                y2={height - axisOffset - yAxisLength * 0.9 * props.currentPay / Math.max(props.comparisonPay, props.currentPay)}
                 stroke={props.comparisonPay > props.currentPay ? "red" : "green"}
                 strokeWidth={barWidth}
             />
@@ -61,10 +61,10 @@ function Graph(props) {
             </text>
             <text
                 x={axisOffset - verticalTextPadding}
-                y={height - axisOffset - (axisLength)/2}
+                y={height - axisOffset - (yAxisLength)/2}
                 dominantBaseline="middle"
                 textAnchor="middle"
-                transform={`rotate(270 ${axisOffset - verticalTextPadding},${height - axisOffset - (axisLength)/2})`}
+                transform={`rotate(270 ${axisOffset - verticalTextPadding},${height - axisOffset - (yAxisLength)/2})`}
                 >
                 Real terms pay
             </text>
