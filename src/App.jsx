40dec065bd5f3971e22970acfa7e679c1ea26e7a
Harry Moss <harryjohnmoss@gmail.com>
2023-06-12 10:40:53 +0100

    Add sharing modal (incomplete)

--- src/App.jsx
+++ src/App.jsx
@@ -40,6 +40,51 @@ function App() {
 
   return (
     <div className="App">
+      <div id="shareModal" className="modal show" tabIndex="-1" style={{ "display": "block" }}>
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Share to Twitter</h5>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+              <p>Select how much detail you want to share</p>
+              <div class="form-check">
+                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" />
+                <label class="form-check-label" for="flexRadioDefault1">
+                  Just a link
+                </label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" />
+                <label class="form-check-label" for="flexRadioDefault2">
+                  A link with a bar chart
+                </label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault3" />
+                <label class="form-check-label" for="flexRadioDefault3">
+                  Full details
+                </label>
+              </div>
+              <img></img>
+              <img></img>
+              <img></img>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href={`https://twitter.com/intent/tweet?text=${encodeURI("See the impact of inflation on your pay")}&url=${encodeURI("https://whatsyourwageworth.com")}&hashtags=costofliving`}
+                type="button"
+                className="btn btn-primary">
+                Share
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
       <form onSubmit={onSubmit}>
         <fieldset>
           <div className='row'>
@@ -123,6 +168,7 @@ function App() {
                   {isPayRise ? <p>This is less than the {nominalTermsPayChange}% nominal pay rise the numbers suggest</p> : null}
                   {!isPayRise ? <p>Since {comparisonYear} you have had a real terms <span className="text-danger">pay cut</span> of {realTermsPayChange}%</p> : null}
                   {<p>Your {comparisonYear} pay is equivalent to {formatCurrency(comparisonPayInCurrentTerms)} today</p>}
+                  <button className="btn btn-primary btn-lg" data-bs-toggle="modal" data-bs-target="#shareModal">Share</button>
                 </div>
               </div>
             </div>
