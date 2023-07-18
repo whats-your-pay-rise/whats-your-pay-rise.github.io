a2789bf9dd6fbe73e321e208f84976e49fbd04d1
Harry Moss <11772383+12v@users.noreply.github.com>
2023-07-19 00:03:08 +0100

    Update typography for different sizes

--- src/App.jsx
+++ src/App.jsx
@@ -36,71 +36,65 @@ function App() {
     setCurrentPay(provisionalCurrentPay);
     setComparisonYear(provisionalComparisonYear);
     setShowResults(true);
-    setTimeout(() => scrollRef.current.scrollIntoView({ behavior: 'smooth' }));
+    setTimeout(() => scrollRef.current.scrollIntoView({ block: "nearest", inline: "nearest", behavior: 'smooth' }));
     event.preventDefault();
   };
 
   return (
-    <Fragment>
-      <div className="container form-container">
-        <div className="row">
-          <div className="">
-            <p className="">Enter your pay below to find out the full cost of inflation. Try your annual salary, your hourly
-              wage, or anything in between.</p>
-            <form onSubmit={onSubmit}>
-              <fieldset>
-                <Select
-                  label="Year you started work"
-                  value={provisionalComparisonYear}
-                  setValue={value => {
-                    setProvisionalComparisonYear(value);
-                    setShowResults(false);
-                  }}
-                  options={data.slice(0, -1).map(entry => entry.year)}
-                />
-                <Input
-                  label={<Fragment>Your pay in <span className="font-weight-bold">{provisionalComparisonYear}</span></Fragment>}
-                  setValue={value => {
-                    setProvisionalComparisonPay(value);
-                    setShowResults(false);
-                  }}
-                />
-                <Input
-                  label={<Fragment>Your pay <span className="font-weight-bold">now</span></Fragment>}
-                  setValue={value => {
-                    setProvisionalCurrentPay(value);
-                    setShowResults(false);
-                  }}
-                />
-                <button type="submit" className="btn btn-calculate btn-primary" disabled={!(provisionalCurrentPay && provisionalComparisonPay)}>CALCULATE</button>
-              </fieldset>
-            </form>
+    <div className="container form-container">
+      <div className="row">
+        <div className="col-12 col-lg-6 form-col">
+          <p className="">Enter your pay below to find out the full cost of inflation. Try your annual salary, your hourly
+            wage, or anything in between.</p>
+          <form onSubmit={onSubmit}>
+            <fieldset>
+              <Select
+                label="Year you started work"
+                value={provisionalComparisonYear}
+                setValue={value => {
+                  setProvisionalComparisonYear(value);
+                  setShowResults(false);
+                }}
+                options={data.slice(0, -1).map(entry => entry.year)}
+              />
+              <Input
+                label={<Fragment>Your pay in <span className="font-weight-bold">{provisionalComparisonYear}</span></Fragment>}
+                setValue={value => {
+                  setProvisionalComparisonPay(value);
+                  setShowResults(false);
+                }}
+              />
+              <Input
+                label={<Fragment>Your pay <span className="font-weight-bold">now</span></Fragment>}
+                setValue={value => {
+                  setProvisionalCurrentPay(value);
+                  setShowResults(false);
+                }}
+              />
+              <button type="submit" className="btn btn-calculate btn-primary" disabled={!(provisionalCurrentPay && provisionalComparisonPay)}>CALCULATE</button>
+            </fieldset>
+          </form>
+        </div>
+        <div className={`${showResults ? "col-12" : "d-none d-lg-block"} col-lg-5 offset-lg-1`} ref={scrollRef} style={showResults ? {} : { filter: "blur(4px) grayscale(1) contrast(0.6)" }}>
+          <div className='results-graph'>
+            <Graph
+              comparisonPay={showResults ? comparisonPayInCurrentTerms : 10000}
+              currentPay={showResults ? currentPay : 8000}
+              comparisonYear={showResults ? comparisonYear : 2010}
+              currentYear={currentYear}
+            />
           </div>
-        </div >
+          <div className="text-center">
+            {isPayRise ? <p>Since {comparisonYear} you have had a real terms pay&nbsp;rise of {realTermsPayChange}%.</p> : null}
+            {isPayRise ? <p>This is less than the {nominalTermsPayChange}% nominal pay rise you might have expected.</p> : null}
+            {!isPayRise ? <p>Since {comparisonYear} you have had a real terms <span className="text-danger">pay&nbsp;cut</span> of {realTermsPayChange}%.</p> : null}
+            {<p>Your {formatCurrency(comparisonPay)} pay in {comparisonYear} is equivalent to {formatCurrency(comparisonPayInCurrentTerms)} today.</p>}
+            {/* <a target="_blank" rel="noreferrer" className="btn btn-primary btn-lg" href="https://twitter.com/intent/tweet?text=Know%20how%20inflation%20has%20impacted%20your%20pay%3F%0A%0AFind%20out%20at%20https%3A%2F%2Fwhatsyourwageworth.com%2C%20you%20may%20be%20surprised!" role="button">Share on Twitter</a> */}
+          </div>
+        </div>
       </div >
-      {showResults ?
-        <div ref={scrollRef} className="container results-container">
-          <div className="row">
-            <div className='results-card'>
-              <div className='results-graph'>
-                <Graph
-                  comparisonPay={comparisonPayInCurrentTerms}
-                  currentPay={currentPay}
-                  comparisonYear={comparisonYear}
-                  currentYear={currentYear}
-                />
-              </div>
-              <div className="">
-                {isPayRise ? <p>Since {comparisonYear} you have had a real terms pay&nbsp;rise of {realTermsPayChange}%.</p> : null}
-                {isPayRise ? <p>This is less than the {nominalTermsPayChange}% nominal pay rise you might have expected.</p> : null}
-                {!isPayRise ? <p>Since {comparisonYear} you have had a real terms <span className="text-danger">pay&nbsp;cut</span> of {realTermsPayChange}%.</p> : null}
-                {<p>Your {formatCurrency(comparisonPay)} pay in {comparisonYear} is equivalent to {formatCurrency(comparisonPayInCurrentTerms)} today.</p>}
-                {/* <a target="_blank" rel="noreferrer" className="btn btn-primary btn-lg" href="https://twitter.com/intent/tweet?text=Know%20how%20inflation%20has%20impacted%20your%20pay%3F%0A%0AFind%20out%20at%20https%3A%2F%2Fwhatsyourwageworth.com%2C%20you%20may%20be%20surprised!" role="button">Share on Twitter</a> */}
-              </div>
-            </div>
-          </div>
-        </div> : null}
-    </Fragment>
+    </div >
+
   );
 }
 
